776a331f47523ee76d6792b47fe49e28c2358488
Justin DeBo <justin.debo@slalom.com>
2023-03-07 19:36:39 -0600

    what if we just get rid of this

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_emr_studio" "this" {
   name                        = "mba-studio"
   service_role                = aws_iam_role.emr_service.arn
   subnet_ids                  = data.aws_subnets.controltower.ids
-  user_role                   = aws_iam_role.emr_user.arn
+  # user_role                   = aws_iam_role.emr_user.arn
   vpc_id                      = data.aws_vpc.controltower.id
   workspace_security_group_id = aws_security_group.this.id
   depends_on = [
